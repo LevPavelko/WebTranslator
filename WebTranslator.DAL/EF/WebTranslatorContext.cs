f90873959ff87c41197b2eaae8ccb806406f6d55
LevPavelko <lev.pavelko.studies@gmail.com>
2024-11-06 20:13:04 +0100

    creating context for my sql

---
+++ WebTranslator.DAL/EF/WebTranslatorContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using WebTranslator.DAL.Entity;
+
+namespace WebTranslator.DAL;
+
+public class WebTranslatorContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public WebTranslatorContext(DbContextOptions<WebTranslatorContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+
+    }
+    // public WebTranslatorContext(DbContextOptions<WebTranslatorContext> options) : base(options)
+    // {
+    //     if (Database.EnsureCreated())
+    //     {
+    //         User user = new User
+    //         {
+    //             Id = Guid.NewGuid(),
+    //             FirstName = "Иван",
+    //             LastName = "Иванович",
+    //             Password = "123456",
+    //             Salt = "123456",
+    //
+    //
+    //         };
+    //         Users.Add(user);
+    //         SaveChanges();
+    //     }
+    // }
+   
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // { 
+    //     if (!optionsBuilder.IsConfigured) // Check if it's already configured
+    //     {
+    //         
+    //         var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+    //         optionsBuilder.UseLazyLoadingProxies()
+    //             .UseMySql("server=localhost;user=root;password=adminroot;database=WebTranslator", serverVersion);
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Web Translator isn't configured");
+    //     }
+    // }
+
+    
+    
+}
